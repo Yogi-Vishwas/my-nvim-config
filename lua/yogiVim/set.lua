76baa47635a7ae85929b6e31212466ac8aa95f84
Vishwas Yogi <vishwas.yogi@sourcefuse.com>
2024-01-21 15:26:23 +0530

    add comments set.lua

--- lua/yogiVim/set.lua
+++ lua/yogiVim/set.lua
@@ -1,13 +1,18 @@
+-- enables fat cursor
 vim.opt.guicursor = ""
+
+-- enables line & relative line number
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- different tab settings
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+-- not highlight every search term use incremental search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
